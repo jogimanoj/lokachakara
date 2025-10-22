28a4dff1126177eba3c635c32e6aa2d719f26a0b
manoj jogi <jogimanoj241@gmail.com>
2025-10-22 12:57:13 +0530

    Add initial CSS styles for webpage layout

---
+++ OneDrive/Desktop/portfolio-webpage/script.js
@@ -0,0 +1,34 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #222;
+  color: #fff;
+  padding: 1em;
+  text-align: center;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+}
+
+section {
+  padding: 2em;
+}
+
+.project {
+  display: inline-block;
+  margin: 1em;
+  text-align: center;
+}
